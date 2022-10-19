92b7f2b38e37fd93196025a1421e15167a975971
cai0913 <1398372203@qq.com>
2022-10-19 19:46:35 +0800

    发现校验的问题

--- big_event/event/src/views/article/artList.vue
+++ big_event/event/src/views/article/artList.vue
@@ -33,7 +33,7 @@
             <!-- 发布文章的对话框 -->
         <el-form :model="pubForm" :rules="pubFormRules" ref="pubFormRef" label-width="100px">
             <el-form-item label="文章标题" prop="title">
-                <el-input v-model="pubForm.title" placeholder="请输入标题"></el-input>
+                    <el-input v-model="pubForm.title" placeholder="请输入标题"></el-input>
             </el-form-item>
             <el-form-item label="文章分类" prop="cate_id">
                 <el-select v-model="pubForm.cate_id" placeholder="请选择分类" style="width: 100%;">
@@ -42,9 +42,20 @@
                 </el-select>
             </el-form-item>
             <el-form-item label="文章内容" prop="content">
-                <quill-editor v-model="pubForm.content">
-                    
-                </quill-editor>
+                <quill-editor v-model="pubForm.content"></quill-editor>
+            </el-form-item>
+            <el-form-item label="文章封面">
+                    <!-- 用来显示封面的图片 -->
+                    <img src="../../assets/images/cover.jpg" alt="" class="cover-img" ref="imgRef" />
+                    <br />
+                    <!-- 文件选择框，默认被隐藏 -->
+                    <input type="file" style="display: none;" accept="image/*" ref="iptFileRef" @change="changeCoverFn"/>
+                    <!-- 选择封面的按钮 -->
+                    <el-button type="text" @click="selCoverFn">+ 选择封面</el-button>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="pubArticleFn('已发布')">发布</el-button>
+                <el-button type="info" @click="pubArticleFn('草稿')">存为草稿</el-button>
             </el-form-item>
         </el-form>
         </el-dialog>
@@ -53,6 +64,16 @@
 
 <script>
 import {getArtCateListAPI} from '@/api'
+// 标签和样式中，引入图片文件直接写“静态路径”（把路径放在js的vue变量里再赋予是不行的）
+// 原因：webpack分析标签时，如果src的值是一个相对路径，他会去帮我们找到哪个路径的文件并一起打包，打包时，会分析文件的大小，小图转成base64字符串再赋予给src，如果是大图拷贝图片换个路径给img的src显示（运行中）
+
+// Vue变量中路径，赋予给标签，都会当作普通的字符串使用
+// 以前：我们写的路径是在vscode看着文件夹写的（以前好用的原因：用live server/磁盘双击打开，都能通过你的相对路径，在指定路径文件夹下，找到图片文件真身）
+// 现在：写的模版代码，是要被webpack翻译处理转换的，你vscode里的代码，转换打包到内存中/dist下，相对路径就会变化，运行时，你写的固定路径字符串就找不到哪个文件真身了
+
+// 注意：只有相对路径本地图片需要注意，如果是一个https://外链图片地址，就可以随便用
+// 直接在标签里写也行，或者在js用变量保存后赋予给标签 都ok，因为运行时，浏览器发现src地址是外链就不找本地文件夹了
+import imgObj from '../../assets/images/cover.jpg'
 export default {
     name:'ArtList',
     data(){
@@ -67,7 +88,9 @@ export default {
             pubForm:{ //表单的数据对象
                 title:'', //文章标题
                 cate_id:'', //文章分类id
-                content:'' //文章内容
+                content:'', //文章内容
+                cover_img:'', //文章封面（保存的是个文件）
+                state: '' // 文章的发布状态，可选值有两个：草稿、已发布
             },
             pubFormRules:{//验证规则对象
                 title:[
@@ -78,7 +101,7 @@ export default {
                     {required:true,message:'请选择文章标题',triggle:'blur'}
                 ],
                 content:[
-                    {required:true,message:'',triggle:'blur'}
+                    {required:true,message:'请输入文章内容',triggle:'blur'}
                 ]
             },
             cateList:[] //保存文章分类列表
@@ -121,6 +144,41 @@ export default {
         async getCateFn(){
             const {data:res} = await getArtCateListAPI()
             this.cateList = res.data
+        },
+        //选择封面点击事件 -> 让文件选择的窗口出现
+        selCoverFn(){
+            this.$refs.iptFileRef.click() //用js来模拟一次点击事件动作（让图片选择窗口出现）
+        },
+        // 用户选择了封面文件
+        changeCoverFn(e){
+            // e.target 拿到触发事件的那个标签（input标签对象本身）
+            // e.target.files 拿到选择的文件数组
+            const files = e.target.files //获取用户选择的文件列表
+            if(files.length === 0){
+                // 用户没有选择封面，把cover_img置空
+                this.pubForm.cover_img = null
+                // img要显示回默认图片
+                this.$refs.imgRef.setAttribute('src', imgObj)
+            }else{
+                // 用户选择了封面
+                this.pubForm.cover_img = files[0]
+                // 把图片文件，要显示到img标签里
+                const url = URL.createObjectURL(files[0])
+                this.$refs.imgRef.setAttribute('src', url)
+            }
+        },
+        // 表单里(点击发布/存为草稿)按钮点击事件-> 准备调用后端接口
+        pubArticleFn(str){
+            // str的值“已发布”，或者“草稿”（后端要求的参数值）
+            this.pubForm.state = str //保存到统一表单对象上
+            // 兜底校验
+            this.$refs.pubFormRef.validate(async valid => {
+                if(valid){
+                    // 都通过
+                }else{
+                    return false //阻止默认行为（按钮有默认的提交行为）
+                }
+            })
         }
 
     }
@@ -128,6 +186,9 @@ export default {
 </script>
 
 <style lang="less" scoped>
+// scoped属性作用：让style里的选择器，只能选中当前组件的标签（为了保证样式的独立性，不影响别的组件）
+// scoped原理：webpack打包时，会给组件标签上添加相同data-v-hash值，然后也会给所有选择器后面加上一个[data-v-hash]值的属性选择器
+// 例：<标签 data-v-390246 calss="my_a"></标签> 选择器会变成.my_a[data-v-390246]
 .search-box {
   display: flex;
   justify-content: space-between;
@@ -141,7 +202,31 @@ export default {
 // ::v-deep作用: 穿透选择, 正常style上加了scope的话, 会给.ql-editor[data-v-hash]属性, 只能选择当前页面标签或者组件的根标签
 // 如果想要选择组件内的标签(那些标签没有data-v-hash值)所以正常选择选不中, 加了::v-deep空格前置的话, 选择器就会变成如下形式
 // [data-v-hash] .ql-editor 这样就能选中组件内的标签的class类名了
+
+
+// ******重要注意事项：
+    // scoped只会给当前组件所有原生标签加上data-v-hash值属性，还会给组件根标签添加data-v-hash值属性，组件内标签不会添加
+
+
+// .ql-editor{
+//     min-height: 300px;
+// }
+
+// 这样写选不中富文本组件内的标签的原因：
+// 写在这里会被后面加上[data-v-hash]属性选择器，而他对应的哪个标签组件内标签，scoped不会给他加上data-v-hash值属性，所以属性选择器选不中
+// 解决：Vue提供了一个::v-deep样式语法，设置后，可以把属性选择器被自动添加到左侧
+// 例：[data-v-hash值] .ql-editor
 ::v-deep .ql-editor {
+    // 最小高度：标签本身的高度考内容撑开，但是内容没有300高度，标签会设置最小高度300px，若内容大于300px，标签高度也会随着撑开（比300px大）
+    // 直接给height：无论容器内的内容有多少，超出300高度的内容会溢出到外面而不是撑开此容器
   min-height: 300px;
 }
+// 设置图片封面的宽高
+.cover-img {
+  width: 400px;
+  height: 280px;
+  object-fit: cover;
+}
+
+// 总结：scoped不会给组件内标签添加data-v属性，所以要用::v-deep穿透选择组件内标签设置样式
 </style>
