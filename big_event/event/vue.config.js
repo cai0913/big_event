e8bd1e67e2d6d5ec6a545479a57088ceb7e165ac
cai0913 <1398372203@qq.com>
2022-10-22 15:24:36 +0800

    打包发布，publicPath值的修改，文件目录自动适配生产环境和开发环境

--- big_event/event/vue.config.js
+++ big_event/event/vue.config.js
@@ -1,5 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
-    publicPath: './'
+    // 影响打包时，index.html引入其他资源的前缀地址
+    // ./可以让开发环境和生产环境都可以正常使用
+    // 为了严谨一些
+    // 开发环境：'/'
+    // 生产环境：'./'
+    // 问题：有无代码可以让脚手架自己识别当前运行环境？
+    // node里有个内置的环境变量process.env.NODE_ENV
+    // process.env.NODE_ENV他会根据你敲击的命令，来使用不同的值
+    // 解决：
+    // 如果你敲击npm run serve，process.env.NODE_ENV的值就是'development'字符串
+    // 如果你敲击npm run build，process.env.NODE_ENV的值就是'production'字符串
+    publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
 })
